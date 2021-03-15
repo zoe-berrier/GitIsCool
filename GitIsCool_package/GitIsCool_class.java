dc8cf68cecd5e24662d5e6f1cf690b1314605ab7
ZebraBerrier <ZebraBerrier@localhost>
2021-03-15 14:38:23 -0500

    uploading final project

---
+++ GitIsCool_package/GitIsCool_class.java
@@ -0,0 +1,18 @@
+package GitIsCool_package;
+/**
+ * the class prints git is cool
+ * @author Zoe Berrier
+ *
+ */
+public class GitIsCool_class {
+	
+	/**
+	 * where the program prints git is cool
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Git is cool!");
+
+	}
+
+}
